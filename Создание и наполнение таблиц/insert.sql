9a779d363eac980c0d421e9027a2b076a01e7c3f
Ashab <asxabal7@gmail.com>
2025-01-26 09:20:15 +0300

    fix/ split it into two files

--- Создание и наполнение таблиц/insert.sql
+++ Создание и наполнение таблиц/insert.sql
@@ -1,36 +1,3 @@
-CREATE TABLE balances (
-    id SERIAL PRIMARY KEY,
-    balances_type_id bigint NOT NULL
-);
-
-CREATE TABLE balance_types (
-    id SERIAL PRIMARY KEY,
-    name varchar(80) UNIQUE NOT NULL
-);
-
-CREATE TABLE transactions (
-    id SERIAL PRIMARY KEY,
-  	quantity int NOT NULL, 
-    balance_id bigint NOT NULL, 
-    transaction_type_id bigint NOT NULL, 
-    currency_id bigint NOT NULL
-);
-
-CREATE TABLE transaction_types(
-    id SERIAL PRIMARY KEY,
-    name varchar(80) UNIQUE NOT NULL
-);
-
-CREATE TABLE currencies(
-    id SERIAL PRIMARY KEY,
-    name varchar(80) UNIQUE NOT NULL
-);
-
-
--- 
--- 
--- 
-
 INSERT INTO currencies (name) VALUES
     ('Поинты'),
     ('Бонусы') RETURNING id, name;
