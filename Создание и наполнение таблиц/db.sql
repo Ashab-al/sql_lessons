bc5e2b0ea5f1fc4c3191595e858deeaba0368d28
Ashab <77648621+Ashab-al@users.noreply.github.com>
2025-01-27 11:03:47 +0300

    create tables (#1)
    * create tables
    
    * fix/ edit sql request
    
    * fix/ edit validates
    
    * fix/ split it into two files
    
    * fix/ add setval

---
+++ Создание и наполнение таблиц/db.sql
@@ -0,0 +1,28 @@
+CREATE TABLE balances (
+    id SERIAL PRIMARY KEY,
+    balances_type_id bigint NOT NULL
+);
+
+CREATE TABLE balance_types (
+    id SERIAL PRIMARY KEY,
+    name varchar(80) UNIQUE NOT NULL
+);
+
+CREATE TABLE transactions (
+    id SERIAL PRIMARY KEY,
+  	quantity int NOT NULL, 
+    balance_id bigint NOT NULL, 
+    transaction_type_id bigint NOT NULL, 
+    currency_id bigint NOT NULL
+);
+
+CREATE TABLE transaction_types(
+    id SERIAL PRIMARY KEY,
+    name varchar(80) UNIQUE NOT NULL
+);
+
+CREATE TABLE currencies(
+    id SERIAL PRIMARY KEY,
+    name varchar(80) UNIQUE NOT NULL
+);
+
