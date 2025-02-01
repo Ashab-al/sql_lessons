e32f537b43d5f77a6eb40661b2107ca93cf5e74c
Ashab <asxabal7@gmail.com>
2025-02-01 03:50:10 +0300

    fix/add exists

--- select.sql
+++ select.sql
@@ -12,30 +12,25 @@ from balances;
 
 -- Вывести балансы, по которым были операции только в рублях
 select * from balances 
-	where 
-      (select count(*)
-          from transactions 
-      		where transactions.balance_id = balances.id and transactions.currency_id = (
-            	select currencies.id from currencies where currencies.name = 'RUB'
-            ))
-    = 
-      (select count(*)
-          from transactions 
-      		where transactions.balance_id = balances.id);
+where
+NOT EXISTS (
+        select transactions.id from transactions 
+        where 
+        transactions.currency_id IN (
+                select currencies.id from currencies
+                where currencies.name != 'RUB'
+                ) and transactions.balance_id = balances.id
+                );
 
 -- Вывести рублевые транзакции заданного типа по балансам заданного типа
-select * from transactions 
-	where transactions.id IN (
-    	select balances.id 
-      	from balances 
-      		where balances.balances_type_id = (
-            	select balance_types.id from balance_types where balance_types.name = 'Основной счет'
-            )
-    
-    ) and 
-    transactions.transaction_type_id = (
-    	select transaction_types.id from transaction_types where transaction_types.name = 'Пополнение'
-    );
-    
-    	
-		     
+select * from transactions
+WHERE 
+NOT EXISTS (select currencies.id from currencies where currencies.name != 'RUB' and transactions.currency_id = currencies.id)
+and 
+NOT EXISTS (select transaction_types.id from transaction_types where transaction_types.name != 'Пополнение' and transactions.transaction_type_id = transaction_types.id)
+and
+EXISTS (select balances.id from balances 
+        where balances.balances_type_id = (
+          select balance_types.id from balance_types where balance_types.name = 'Основной счет'
+          ) and balances.id = transactions.balance_id
+          )
